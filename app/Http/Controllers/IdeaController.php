b47179c17709e61e40f0a776274c375ba009ec6b
Leandro.Carolino <carolino.leandro@gmail.com>
2024-07-07 16:37:35 +0200

    add update func

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -7,25 +7,47 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
+    public function show(Idea $idea){    
+        return view("ideas.show", compact("idea"));
+    }
     //
     public function store(){
         request()->validate([
-            "idea"=> "required|min:3|max:240",
+            "content"=> "required|min:3|max:240",
         ]);
         $idea = Idea::create(
             [
-                'content' => request()->get('idea', ''),
+                'content' => request()->get('content', ''),
             ]
         );
         return redirect()->route('dashboard')->with('success','Idea created bruh!');
     }
 
-    public function destroy($id)
+    public function destroy(Idea $idea)
     {
         // Find and delete the idea
-        Idea::where('id', $id)->firstOrFail() ->delete();
+        // this is an example of two options that work the same
+        // Idea::where('id', $id)->firstOrFail() ->delete();
+        $idea->delete();
       
 
         return redirect()->route('dashboard')->with('success', 'Idea deleted successfully');
     }
-}
+
+    public function edit(Idea $idea){  
+        $editing = true;  
+        return view("ideas.show", compact("idea" ,'editing'));
+    }
+
+    public function update(Idea $idea){
+        request()->validate(
+            [
+                'content' => 'required|min:3|max:240'
+            ]
+            );
+            $idea->content = request()->get('content', '');
+            $idea->save();
+
+            return redirect()->route('ideas.show', $idea->id)->with('success','Idea edited broski');
+    }
+}   
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ DashboardController::class,'index'])->name('dashboard');
-Route::post('/idea',[IdeaController::class,'store'])->name('idea.store');
-Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+Route::get('/ideas/{idea}',[IdeaController::class,'show'])->name('ideas.show');
+Route::post('/ideas',[IdeaController::class,'store'])->name('ideas.store');
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::get('/terms', function(){
     return view('terms');
